06b85819d1862f660af18e90705d473a5dc26c16
Gogogohuang <gogogohuang@MacBook-Pro.local>
2015-03-09 00:35:04 +0800

    addd debug printf

--- cdata_ioctl.h
+++ cdata_ioctl.h
@@ -10,6 +10,13 @@
 #define IOCTL_Init          _IO(0xD0, 5)
 #define IOCTL_SetAll        _IOW(0xD0, 6, int)
 
+#define Cdata_Debug_Enable 1
+
+#if  Cdata_Debug_Enable
+#define CDATA_DEBUG_PRINT printf
+#else
+#define CDATA_DEBUG_PRINT (...)
+#endif
 
 #define LEN_OF_NAME 8
 
